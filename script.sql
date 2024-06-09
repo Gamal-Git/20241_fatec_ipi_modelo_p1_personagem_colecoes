8d2b66407d1ea023148d2d6c9105ff5444d93909
Gabriel <emaildogamal@gmail.com>
2024-06-09 15:15:05 -0300

    feat(p2):criacao tb_usuario e usuario padrao

--- script.sql
+++ script.sql
@@ -1,8 +1,14 @@
 -- Active: 1717874623742@@localhost@5432@postgres
 CREATE TABLE tb_atividade (
-cod_atividade SERIAL PRIMARY KEY,     
+cod_atividade INT,     
 descricao VARCHAR(200),     
 data_de_ocorrencia TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
-SELECT * FROM tb_atividade;
+CREATE TABLE tb_usuario (
+    cod_usuario SERIAL PRIMARY KEY,
+    login VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+-- Inserção de um usuário padrão
+INSERT INTO tb_usuario (login, senha) VALUES ('admin', 'admin');
