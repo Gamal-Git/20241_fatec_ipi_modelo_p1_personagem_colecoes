9aa6e37c89ce5eae9ba3763c9b762cf13a79fea7
Gabriel <emaildogamal@gmail.com>
2024-06-09 16:29:54 -0300

    feat(p2): associa atividade a usuario

--- script.sql
+++ script.sql
@@ -1,14 +1,22 @@
 -- Active: 1717874623742@@localhost@5432@postgres
 CREATE TABLE tb_atividade (
-cod_atividade INT,     
-descricao VARCHAR(200),     
-data_de_ocorrencia TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+    cod_atividade INT NOT NULL,     
+    descricao VARCHAR(200) NOT NULL,     
+    data_de_ocorrencia TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tb_usuario (
     cod_usuario SERIAL PRIMARY KEY,
     login VARCHAR(50) NOT NULL,
-    senha VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL,
 );
--- Inserção de um usuário padrão
+
 INSERT INTO tb_usuario (login, senha) VALUES ('admin', 'admin');
+
+
+ALTER TABLE tb_atividade
+ADD COLUMN cod_usuario INT;
+
+ALTER TABLE tb_atividade
+ADD CONSTRAINT fk_usuario
+FOREIGN KEY (cod_usuario) REFERENCES tb_usuario(cod_usuario);
