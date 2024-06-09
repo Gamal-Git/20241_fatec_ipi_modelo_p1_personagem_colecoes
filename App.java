b17c6988237fe2ec7f0e4fef400ff4553f6b3da8
Gabriel <emaildogamal@gmail.com>
2024-06-09 16:28:13 -0300

    fix(p2):correcao de bug na tela de login

--- App.java
+++ App.java
@@ -4,29 +4,27 @@ public class App {
   public static void main(String[] args) {
     var menu = "1-Jogar\n2-Consultar Log\n0-Sair";
     int op = 0;
+    var user = new Usuario();
+    var dao = new PersonagemDAO();
+    do{
+      user.login =JOptionPane.showInputDialog("Login: ");
+      user.senha = JOptionPane.showInputDialog("Senha: ");
+    }while(!dao.loginUsuario(user));
     do{
       try{
-        var user = new Usuario();
-        var daoUser = new PersonagemDAO();
-        do{
-          user.login =JOptionPane.showInputDialog("Login: ");
-          user.senha = JOptionPane.showInputDialog("Senha: ");
-        }while(!daoUser.loginUsuario(user));
           op = Integer.parseInt(
           JOptionPane.showInputDialog(menu)
         );
         switch(op){
           case 1: {
             var p = new Personagem();
-            var dao = new PersonagemDAO();
             for (int i = 0; i < 10; i++) {
                 p.jogar();
-                dao.log(p);
+                dao.log(p, user);
               }
             break;
           }
           case 2:{
-            var dao = new PersonagemDAO();
             dao.consultarLog();
           break;
           }
