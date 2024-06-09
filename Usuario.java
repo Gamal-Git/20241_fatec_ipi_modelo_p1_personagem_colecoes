d37e2119b1d3a89b17333ae74566e4d420c57ee0
Gabriel <emaildogamal@gmail.com>
2024-06-09 15:13:44 -0300

    feat(p2):criacao da classe usuario

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    String login;
+    String senha;
+
+    Usuario(){
+        login = null;
+        senha = null;
+    }
+
+    public String getLogin (){
+        return login;
+    }
+
+    public String getSenha (){
+        return senha;
+    }
+}
