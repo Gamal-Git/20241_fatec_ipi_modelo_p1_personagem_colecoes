2b85913c7cd332f903c3c4a85d7c0fc50257dec8
Gabriel <emaildogamal@gmail.com>
2024-06-09 15:22:02 -0300

    feat(p2):implemantacao da interface de login

--- App.java
+++ App.java
@@ -1,41 +1,50 @@
 import javax.swing.JOptionPane;
 
 public class App {
-    public static void main(String[] args) {
-      var menu = "1-Jogar\n2-Consultar Log";
-      int op = 0;
-      do{
-        try{
+  public static void main(String[] args) {
+    var menu = "1-Jogar\n2-Consultar Log\n0-Sair";
+    int op = 0;
+    do{
+      try{
+        var user = new Usuario();
+        var daoUser = new PersonagemDAO();
+        do{
+          user.login =JOptionPane.showInputDialog("Login: ");
+          user.senha = JOptionPane.showInputDialog("Senha: ");
+        }while(!daoUser.loginUsuario(user));
           op = Integer.parseInt(
-            JOptionPane.showInputDialog(menu)
-          );
-          switch(op){
-            case 1: {
-                var p = new Personagem();
-                var dao = new PersonagemDAO();
-                for (int i = 0; i < 10; i++) {
-                    p.jogar(p);
-                    dao.log(p);
-                }
+          JOptionPane.showInputDialog(menu)
+        );
+        switch(op){
+          case 1: {
+            var p = new Personagem();
+            var dao = new PersonagemDAO();
+            for (int i = 0; i < 10; i++) {
+                p.jogar();
+                dao.log(p);
+              }
             break;
-            }
-            case 2:{
-                var dao = new PersonagemDAO();
-                dao.consultarLog();
-            }
-            default:{
-              JOptionPane.showMessageDialog(null, "Opção inválida");
-            }
+          }
+          case 2:{
+            var dao = new PersonagemDAO();
+            dao.consultarLog();
+          break;
+          }
+          case 0:{
+            JOptionPane.showMessageDialog(null, "Saindo...");
+          break;
+          }
+          default:{
+            JOptionPane.showMessageDialog(null, "Opção inválida");
           }
         }
-        catch(Exception e){
-          e.printStackTrace();
-          JOptionPane.showMessageDialog(
-            null, "Coisas ruins aconteceram..."
-          );
-        }
-
-      }while(op != 0);   
-    }
-    
+      }
+      catch(Exception e){
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(
+          null, "Coisas ruins aconteceram..."
+        );
+      }
+    }while(op != 0);   
+  }  
 }
