663dfb86a1282ee5d63e8035bc1c6947c48687eb
Gabriel <emaildogamal@gmail.com>
2024-06-11 19:08:52 -0300

    docs(p2): comentario aiven excluido

--- ConnectionFactory.java
+++ ConnectionFactory.java
@@ -6,10 +6,7 @@ public class ConnectionFactory {
   private static String db = "postgres";
   private static String user = "postgres";
   private static String password = "123456";
-  //https://google.com:80/search
-  //string de conexão
-  //jdbc:postgresql://localhost:5432/20241_fatec_ipi_poo_pessoas
-  //jdbc:postgresql://pgfatec-374536b-fatec-c2f0.e.aivencloud.com:28190/defaultdb?ssl=require&user=avnadmin&password=AVNS_XIthS_n0LgSbuKwcyg8
+
   public static Connection getConnection() throws Exception{
     //cláusula catch or declare
       var s = String.format(
