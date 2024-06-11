318571556d6d25f760ca85d026887ddd6228e17c
Gabriel <emaildogamal@gmail.com>
2024-06-11 18:26:31 -0300

    feat(p2): implementacao dos metodos de ranking

--- App.java
+++ App.java
@@ -2,26 +2,27 @@ import javax.swing.JOptionPane;
 
 public class App {
   public static void main(String[] args) {
-    var menu = "1-Jogar\n2-Consultar Log\n0-Sair";
     int op = 0;
     var user = new Usuario();
     var dao = new PersonagemDAO();
+    var p = new Personagem();
     do{
       user.login =JOptionPane.showInputDialog("Login: ");
       user.senha = JOptionPane.showInputDialog("Senha: ");
-    }while(!dao.loginUsuario(user));
-    do{
-      try{
+      }while(!dao.loginUsuario(user));
+      do{
+        try{
+          var menu = "1-Jogar\n2-Consultar Log\n0-Sair";
           op = Integer.parseInt(
-          JOptionPane.showInputDialog(menu)
+            JOptionPane.showInputDialog(menu)
         );
         switch(op){
           case 1: {
-            var p = new Personagem();
             for (int i = 0; i < 10; i++) {
-                p.jogar();
+                p.jogar(user);
                 dao.log(p, user);
               }
+              dao.ranking(user, p);
             break;
           }
           case 2:{
@@ -34,8 +35,10 @@ public class App {
           }
           default:{
             JOptionPane.showMessageDialog(null, "Opção inválida");
+          break;
           }
         }
+        user.zerarPontuacao();
       }
       catch(Exception e){
         e.printStackTrace();
